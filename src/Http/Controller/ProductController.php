8164ad89f27cf049689b9deb24ea9a0dd5db0630
GrzegorzW <grzegorz.waszkowiak@gmail.com>
2018-01-09 19:52:20 +0100

    change product price

--- src/Http/Controller/ProductController.php
+++ src/Http/Controller/ProductController.php
@@ -5,16 +5,21 @@ declare(strict_types = 1);
 namespace Gogart\Http\Controller;
 
 use Gogart\Application\Product\Command\AddProductCommand;
+use Gogart\Application\Product\Command\ChangeProductPriceCommand;
 use Gogart\Application\Product\Command\ChangeProductTitleCommand;
+use Gogart\Application\Product\Data\PriceData;
 use Gogart\Application\Product\Data\ProductData;
 use Gogart\Application\Product\Data\TitleData;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Prooph\ServiceBus\CommandBus;
+use Prooph\ServiceBus\Exception\CommandDispatchException;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProductController
 {
@@ -44,6 +49,9 @@ class ProductController
      * @param ProductData $data
      *
      * @return Response
+     *
+     * @throws CommandDispatchException
+     * @throws BadRequestHttpException
      */
     public function add(ProductData $data): Response
     {
@@ -83,10 +91,36 @@ class ProductController
      * @param TitleData $data
      *
      * @return Response
+     *
+     * @throws CommandDispatchException
+     * @throws BadRequestHttpException
+     * @throws NotFoundHttpException
      */
     public function changeTitle(UuidInterface $productId, TitleData $data): Response
     {
         $command = new ChangeProductTitleCommand([
+            'id' => $productId,
+            'data' => $data
+        ]);
+
+        $this->commandBus->dispatch($command);
+
+        return new JsonResponse('', Response::HTTP_NO_CONTENT, [], true);
+    }
+
+    /**
+     * @param UuidInterface $productId
+     * @param PriceData $data
+     *
+     * @return Response
+     *
+     * @throws NotFoundHttpException
+     * @throws CommandDispatchException
+     * @throws BadRequestHttpException
+     */
+    public function changePrice(UuidInterface $productId, PriceData $data): Response
+    {
+        $command = new ChangeProductPriceCommand([
             'id' => $productId,
             'data' => $data
         ]);
